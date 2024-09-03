d2a37703537883d73b08989dbc003005574ed3f1
Sameer <sameershreyas13@gmail.com>
2024-09-03 15:43:30 +0530

    about page updated

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className="bg-gradient-to-b from-gray-100 to-gray-300 text-gray-800 py-20 fade-in">
       <div className="container mx-auto px-4 lg:px-32 flex flex-col lg:flex-row items-center">
         {/* Image Section */}
-        <div className="lg:w-1/2 mb-10 lg:mb-0">
+        <div className="lg:w-2/5 w-3/4 mb-10 lg:mb-0">
           <img
             src={aboutImage}
             alt="About Me"
@@ -15,8 +15,8 @@ const About = () => {
         </div>
         
         {/* Text Section */}
-        <div className="lg:w-1/2 lg:pl-12 text-center lg:text-left">
-          <h3 className="text-5xl font-extrabold mb-6">About Me</h3>
+        <div className="lg:w-3/5 lg:pl-12 text-center lg:text-left">
+          <h3 className="text-4xl font-extrabold mb-6">About Me</h3>
           <p className="max-w-2xl mx-auto text-lg leading-relaxed mb-8">
             I'm a final-year B.Tech student specializing in IoT in Electronics and Communication Engineering at NSUT, Delhi. I have experience in full-stack web development, data analysis, and automation. My projects showcase my passion for building scalable and innovative solutions.
           </p>
